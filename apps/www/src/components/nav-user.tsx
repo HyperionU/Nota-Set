60617bdee0e4831982440c006b92c923295f508c
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-02-12 03:53:46 +0000

    (www): work on sidebar (prep for release)

--- apps/www/src/components/nav-user.tsx
+++ apps/www/src/components/nav-user.tsx
@@ -6,7 +6,9 @@ import {
   ChevronsUpDown,
   CreditCard,
   LogOut,
-  Sparkles
+  Moon,
+  Sparkles,
+  Sun
 } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -64,9 +66,9 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="size-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              <Avatar className="size-8 rounded-lg items-center">
+                <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user.name}</span>
