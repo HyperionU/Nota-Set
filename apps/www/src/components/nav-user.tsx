c9005476d5d083b247d289d25fee03378bd25dfc
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-02-15 02:36:14 +0000

    (www): update 'avatar' on sidebar

--- apps/www/src/components/nav-user.tsx
+++ apps/www/src/components/nav-user.tsx
@@ -66,7 +66,7 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="size-8 rounded-lg items-center">
+              <Avatar className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-accent text-sidebar-accent-foreground">
                 <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                 <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               </Avatar>
